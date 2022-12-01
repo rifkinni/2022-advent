54cc3f32138df6f7d54e9a67f66864b60038db25
Nicole Rifkin <rifkin@loseit.com>
2022-12-01 18:04:28 -0500

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,28 @@
+import java.util.Collections.max
+
+class Day01 {
+    fun parseSupplies(input: List<String>): List<Int> {
+        return input.joinToString(",").split(",,").map { collection ->
+            collection.split(",").map { num -> Integer.parseInt(num) }.sum()
+        }
+    }
+}
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val elfSupplies = Day01().parseSupplies(input)
+        return max(elfSupplies)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elfSupplies = Day01().parseSupplies(input)
+        return elfSupplies.sortedDescending().slice(0..2).sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000 )
+    check(part2(testInput) == 45000 )
 
     val input = readInput("Day01")
     println(part1(input))
